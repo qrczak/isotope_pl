83d825ea344d0a3c14541d8d4eff0095eff34061
Qrczak <qrczak@gmail.com>
2009-11-02 22:37:33 +0100

    Poprawki w module.php

--- system/modules/isotope/languages/pl/modules.php
+++ system/modules/isotope/languages/pl/modules.php
@@ -27,14 +27,14 @@
 $GLOBALS['TL_LANG']['MOD']['store']						= 'Isotope eCommerce';
 $GLOBALS['TL_LANG']['MOD']['isotopeMedia']				= array('Isotope File Manager', 'This module allows you to upload new files to the server or to manage existing files and folders for the Isotope file system.');
 $GLOBALS['TL_LANG']['MOD']['orders']					= array('Zamówienia Isotope', '');
-$GLOBALS['TL_LANG']['MOD']['shipping']					= array('Shipping Methods','');
-$GLOBALS['TL_LANG']['MOD']['payment']					= array('Payment Methods','');
-$GLOBALS['TL_LANG']['MOD']['taxes']						= array('Taxes','Set up tax classes and rates.');
-$GLOBALS['TL_LANG']['MOD']['store_configuration']		= array('Store Configuration','');
-$GLOBALS['TL_LANG']['MOD']['products_and_attributes']	= array('Products and Attributes','');
-$GLOBALS['TL_LANG']['MOD']['attribute_manager']			= array('Attribute Manager','');
+$GLOBALS['TL_LANG']['MOD']['shipping']					= array('Metody wysyłki','');
+$GLOBALS['TL_LANG']['MOD']['payment']					= array('Metody płatności','');
+$GLOBALS['TL_LANG']['MOD']['taxes']						= array('Podatki','Ustaw stawki podatków.');
+$GLOBALS['TL_LANG']['MOD']['store_configuration']		= array('Konfiguracja sklepu','');
+$GLOBALS['TL_LANG']['MOD']['products_and_attributes']	= array('Produkty i atrybuty','');
+$GLOBALS['TL_LANG']['MOD']['attribute_manager']			= array('Manager atrybutów','');
 $GLOBALS['TL_LANG']['MOD']['product_type_manager']			= array('Product Type Manager','');
-$GLOBALS['TL_LANG']['MOD']['product_manager']			= array('Product Management','');
+$GLOBALS['TL_LANG']['MOD']['product_manager']			= array('Manager produktów','');
 
 $GLOBALS['TL_LANG']['MOD']['iso_mail']					= array('E-Mail Manager','');
 
@@ -42,8 +42,8 @@ $GLOBALS['TL_LANG']['MOD']['iso_mail']					= array('E-Mail Manager','');
  * Front end modules									
  */ 													
 $GLOBALS['TL_LANG']['FMD']['isoFilterModule']			= array('Isotope Filter Module', 'Define individual filters for Isotope such as category trees and product attribute filters.');
-$GLOBALS['TL_LANG']['FMD']['isoProductLister']			= array('Isotope Product Lister', 'General Listing module.  Can be used to list products or values of attributes. May be combined with other modules (i.e. the Filter Module) to provide further drill-down capabilities.');
-$GLOBALS['TL_LANG']['FMD']['isoProductReader']			= array('Isotope Product Reader', 'Product reader module.  This is used to display product details.');
+$GLOBALS['TL_LANG']['FMD']['isoProductLister']			= array('Lista produktów Isotope', 'General Listing module.  Can be used to list products or values of attributes. May be combined with other modules (i.e. the Filter Module) to provide further drill-down capabilities.');
+$GLOBALS['TL_LANG']['FMD']['isoProductReader']			= array('Czytnik produktu Isotope', 'Product reader module.  This is used to display product details.');
 $GLOBALS['TL_LANG']['FMD']['isoShoppingCart']			= array('Isotope Shopping Cart', 'A fully-featured shopping cart module.  Box or Full Display can be set by template selection.');
 $GLOBALS['TL_LANG']['FMD']['isoCheckout']				= array('Isotope Checkout Module', 'Allow store customers to complete their transactions.');
 $GLOBALS['TL_LANG']['FMD']['isoGiftRegistryManager']	= array('Isotope Gift Registry Manager', 'A fully-featured gift registry module.  Allows users to create and manage their own gift registries.');
@@ -51,5 +51,5 @@ $GLOBALS['TL_LANG']['FMD']['isoGiftRegistrySearch']		= array('Isotope Gift Regis
 $GLOBALS['TL_LANG']['FMD']['isoGiftRegistryResults']	= array('Isotope Gift Registry Search Results', 'Allow store customers to search for existing Gift Registries');
 $GLOBALS['TL_LANG']['FMD']['isoGiftRegistryReader']		= array('Isotope Gift Registry Reader', 'Allow store customers to view existing Gift Registries');
 $GLOBALS['TL_LANG']['FMD']['isoGiftRegistry']			= array('Isotope Gift Registry','Allow customers to set up, manage or shop from already created Gift Registries.');
-$GLOBALS['TL_LANG']['FMD']['isoAddressBook']			= array('Isotope Address Book','Allow customers to manage their address book.');
+$GLOBALS['TL_LANG']['FMD']['isoAddressBook']			= array('Książka adresowa Isotope','Allow customers to manage their address book.');
 
